3ebd8baf561fb9a9c39be831c80b62cd576f1e96
simon <sbalfedev@gmail.com>
2026-02-19 22:17:45 +0000

    remove

--- apps/web/.wrangler/tmp/deploy-UAW8WI/index.js
+++
@@ -1,9 +0,0 @@
-import { z, A } from "./assets/worker-entry-BXRWiTmP.js";
-import "node:events";
-import "node:stream";
-import "node:async_hooks";
-import "node:stream/web";
-export {
-  z as createServerEntry,
-  A as default
-};
